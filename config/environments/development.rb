5646c3f85c7bc4657ce99c1678f666f38fd42b48
Murat Gocmen <murat.dbc@gmail.com>
2015-01-06 12:42:00 -0800

    removed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,14 +37,5 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-   :tls => true,
-   :address => "smtp.gmail.com",
-   :port => "587",
-   :domain => "gmail.com",
-   :authentication => :login,
-   :user_name => "murat.dbc@gmail.com",
-   :password => "1qazZAQ!2wsxXSW@"
- }
+
 end
